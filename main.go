97c2ac37cb5f85df4677c590badb3d017c5bd325
Brenton Bills <fud.fud@gmail.com>
2015-07-01 20:05:11 +1000

    Initial yaml configuration parsing

--- main.go
+++ main.go
@@ -1,5 +1,48 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"gopkg.in/yaml.v2"
+)
 
+
+// var data = `
+// a: Easy!
+// b:
+//   c: 2
+//   d: [3, 4]
+// `
+
+// type T struct {
+//         A string
+//         B struct{C int; D []int ",flow"}
+// }
+
+
+var data = `---
+project_name:
+  path: /api/v1/create_article
+  sqs: create_article
+`
+
+type Config struct {
+	name string
+	mapping struct {
+		path string
+		queue string
+	}
+}
+
+
+func main() {
+	config := Config{}
+
+	err := yaml.Unmarshal([]byte(data), &config)
+
+        if err != nil {
+                log.Fatalf("error: %v", err)
+        }
+	
+        fmt.Printf("--- config:\n%v\n\n", config)
 }
