5259b0a3954cd7ae190d29679d65e1d853447cfe
Mark Gandolfo <mark@gandolfo.com.au>
2015-07-04 19:57:42 +1000

    renamed package and things fails for different reasons

--- yaml_test.go
+++ yaml_test.go
@@ -1,24 +1,22 @@
-package main_test
+package main
 
 import (
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-var data = `
+var testData = `
 mappings:
-	project1:
-		path: /api/v1/pathname1
-		sqs: my_sqs_queue1
-	project2:
-		path: /api/v1/pathname2
-		sqs: my_sqs_queue2
+  project1:
+    path: /api/v1/pathname1
+    sqs: my_sqs_queue1
+  project2:
+    path: /api/v1/pathname2
+    sqs: my_sqs_queue2
 `
 
-var expectedString = `
-{Mappings:map[project1:{Path:/api/v1/pathname1 Sqs:my_sqs_queue1} project2:{Path:/ap
-i/v1/pathname2 Sqs:my_sqs_queue2}]}`
+var expectedString = `main.Config{Mappings:map[string]main.Route{"project1":main.Route{Path:"/api/v1/pathname1", Sqs:"my_sqs_queue1"}, "project2":main.Route{Path:"/api/v1/pathname2", Sqs:"my_sqs_queue2"}}}`
 
 func TestParser(t *testing.T) {
-	assert.True(t, parseYaml(data), expectedString)
+	assert.Equal(t, parseYaml(testData), expectedString)
 }
